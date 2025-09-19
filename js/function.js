8346838ccbd998c52a9a6ac4113c9d594a137edd
María Morales1234 <mjmorales@uniboyaca.edu.co>
2025-09-18 21:39:31 -0500

    correcciones del javascript

--- js/function.js
+++ js/function.js
@@ -1,11 +1,19 @@
-const tablero = document.getElementById('tablero').style.gridTemplateColumns = `repeat(${n}, 80px)`;
+const tablero = document.getElementById('tablero');
 const mensaje = document.getElementById('mensaje');
 let primeraCarta = null;
 let segundaCarta = null;
 let bloquear = false;
 let paresEncontrados = 0;
 let paresTotales = 0;
-const imagenesDisponibles = {};
+
+const imagenesDisponibles = [
+    "img/1.png", "img/2.png", "img/3.png", "img/4.png", "img/5.png", "img/6.png", "img/7.png", "img/8.png", "img/9.png", "img/10.png",
+    "img/11.png", "img/12.png", "img/13.png", "img/14.png", "img/15.png", "img/16.png", "img/17.png", "img/18.png", "img/19.png", "img/20.png",
+    "img/21.png", "img/22.png", "img/23.png", "img/24.png", "img/25.png", "img/26.png", "img/27.png", "img/28.png", "img/29.png", "img/30.png",
+    "img/31.png", "img/32.png", "img/33.png", "img/34.png", "img/35.png", "img/36.png", "img/37.png", "img/38.png", "img/39.png", "img/40.png",
+    "img/41.png", "img/42.png", "img/43.png", "img/44.png", "img/45.png", "img/46.png", "img/47.png", "img/48.png", "img/49.png", "img/50.png",
+];
+
 
 function iniciarJuego(tamaño){
     tablero.innerHTML = "";
@@ -21,11 +29,12 @@ function iniciarJuego(tamaño){
     let cartas = [...seleccionadas, ...seleccionadas];
     cartas.sort(()=> Math.random()- 0.5);
     cartas.forEach(src => {
+        console.log("Cargando imagen:", src); 
         const carta = document.createElement('div');
         carta.classList.add('carta');
         carta.innerHTML = `
-            <div class="caraFrente"></div>
-            <div class="caraAtras"><img src="${src}" alt="imagen"></div>
+            <div class="cara caraFrente"></div>
+            <div class="cara caraAtras"><img src="${src}" alt="imagen"></div>
         `;
         carta.addEventListener('click', ()=> voltearCarta(carta, src));
         tablero.appendChild(carta);
@@ -46,7 +55,7 @@ function voltearCarta(carta, src){
             segundaCarta = null;
             bloquear = false;
             if(paresEncontrados===paresTotales){
-                mensaje.textContent = "¡Felicidades, ganaste en un tablero!" + (Math.sqrt(paresTotales*2)) + "x" + (Math.sqrt(paresTotales*2)) + "!";
+                mensaje.textContent = `¡Felicidades, ganaste en un tablero ${Math.sqrt(paresTotales*2)}x${Math.sqrt(paresTotales*2)} `;
             }
         }else{
             setTimeout(()=>{
@@ -59,4 +68,3 @@ function voltearCarta(carta, src){
         }
     }
 }
-iniciarJuego(4);
