9bf4782fcbac6fcf244ab227e81bacdf7fe9bbfb
María Morales1234 <mjmorales@uniboyaca.edu.co>
2025-09-19 19:11:17 -0500

    temporizador, contador de intentos y records

--- js/function.js
+++ js/function.js
@@ -1,5 +1,12 @@
 const tablero = document.getElementById('tablero');
 const mensaje = document.getElementById('mensaje');
+const imagenesDisponibles = [
+    "img/1.png", "img/2.png", "img/3.png", "img/4.png", "img/5.png", "img/6.png", "img/7.png", "img/8.png", "img/9.png", "img/10.png",
+    "img/11.png", "img/12.png", "img/13.png", "img/14.png", "img/15.png", "img/16.png", "img/17.png", "img/18.png", "img/19.png", "img/20.png",
+    "img/21.png", "img/22.png", "img/23.png", "img/24.png", "img/25.png", "img/26.png", "img/27.png", "img/28.png", "img/29.png", "img/30.png",
+    "img/31.png", "img/32.png", "img/33.png", "img/34.png", "img/35.png", "img/36.png", "img/37.png", "img/38.png", "img/39.png", "img/40.png",
+    "img/41.png", "img/42.png", "img/43.png", "img/44.png", "img/45.png", "img/46.png", "img/47.png", "img/48.png", "img/49.png", "img/50.png",
+];
 
 let primeraCarta = null;
 let segundaCarta = null;
@@ -10,14 +17,6 @@ let intentos = 0;
 let tiempoInicio = null;
 let temporizador = null;
 
-const imagenesDisponibles = [
-    "img/1.png", "img/2.png", "img/3.png", "img/4.png", "img/5.png", "img/6.png", "img/7.png", "img/8.png", "img/9.png", "img/10.png",
-    "img/11.png", "img/12.png", "img/13.png", "img/14.png", "img/15.png", "img/16.png", "img/17.png", "img/18.png", "img/19.png", "img/20.png",
-    "img/21.png", "img/22.png", "img/23.png", "img/24.png", "img/25.png", "img/26.png", "img/27.png", "img/28.png", "img/29.png", "img/30.png",
-    "img/31.png", "img/32.png", "img/33.png", "img/34.png", "img/35.png", "img/36.png", "img/37.png", "img/38.png", "img/39.png", "img/40.png",
-    "img/41.png", "img/42.png", "img/43.png", "img/44.png", "img/45.png", "img/46.png", "img/47.png", "img/48.png", "img/49.png", "img/50.png",
-];
-
 
 function iniciarJuego(tamaño){
     tablero.innerHTML = "";
@@ -26,6 +25,10 @@ function iniciarJuego(tamaño){
     segundaCarta = null;
     bloquear = false;
     paresEncontrados = 0;
+    intentos = 0;
+    if (temporizador) clearInterval(temporizador);
+    tiempoInicio = Date.now();
+    temporizador = setInterval(actualizarTiempo, 1000);
     tablero.style.gridTemplateColumns = `repeat(${tamaño}, 80px)`;
     tablero.style.gridTemplateRows = `repeat(${tamaño}, 80px)`;
     paresTotales = (tamaño*tamaño)/2;
@@ -53,13 +56,14 @@ function voltearCarta(carta, src){
     }else if (!segundaCarta){
         segundaCarta = {carta, src};
         bloquear = true;
+        intentos ++;
         if (primeraCarta.src === segundaCarta.src){
             paresEncontrados++;
             primeraCarta = null;
             segundaCarta = null;
             bloquear = false;
             if(paresEncontrados===paresTotales){
-                mensaje.textContent = `¡Felicidades, ganaste en un tablero ${Math.sqrt(paresTotales*2)}x${Math.sqrt(paresTotales*2)} `;
+                finalizarJuego();
             }
         }else{
             setTimeout(()=>{
@@ -72,3 +76,27 @@ function voltearCarta(carta, src){
         }
     }
 }
+ function actualizarTiempo(){
+    const tiempoActual = Math.floor((Date.now() - tiempoInicio)/100);
+    mensaje.textContent = `tiempo: ${tiempoActual}s | intentos: ${intentos}`;
+ }
+
+ function finalizarJuego(){
+    clearInterval(temporizador);
+    const tiempoFinal = Math.floor((Date.now() - tiempoInicio)/1000);
+    mensaje.textContent = `¡Felicidades, ganaste en un tablero ${Math.sqrt(paresTotales*2)}x${Math.sqrt(paresTotales*2)}`;
+    let mejorTiempo = localStorage.getItem("mejorTiempo");
+    let menorIntentos = localStorage.getItem("menorIntentos");
+    if (!mejorTiempo || tiempoFinal < mejorTiempo){
+        localStorage.setItem("mejorTiempo", tiempoFinal);
+        mensaje.textContent += `|Nuev record de tiempo: ${tiempoFinal}s`;
+    }else{
+        mensaje.textContent = `|Mejor tiempo: ${mejorTiempo}s`;
+    }
+    if (!menorIntentos || intentos < menorIntentos){
+        localStorage.setItem("memorIntentos", intentos);
+        mensaje.textContent += `|Nuevo record de intentos: ${intentos}s`;
+    }else{
+        mensaje.textContent = `|Menor numero de intentos: ${menorIntentos}s`;
+    }
+ }
